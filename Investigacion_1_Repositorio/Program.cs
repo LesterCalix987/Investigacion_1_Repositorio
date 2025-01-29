59abff64976e7c5c78cab8d2b428a98de7b443d5
LesterCalix987 <lester.calix@ujcv.edu.hn>
2025-01-28 21:17:54 -0600

    Agregar archivos de proyecto.

---
+++ Investigacion_1_Repositorio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigacion_1_Repositorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
